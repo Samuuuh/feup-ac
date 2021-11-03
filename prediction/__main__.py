019d638f620a5e09ae05c471e8ede46b7748bed2
Alexandre Abreu <a3brx@tuta.io>
2021-11-03 11:32:04 +0000

    Changed file names and created modules

---
+++ prediction/__main__.py
@@ -0,0 +1,27 @@
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+
+
+def read_frame(name: str):
+    return pd.read_csv('./data/preprocessed/' + name + '.csv', sep=';')
+
+
+def simple():
+    train = read_frame("loan_dev")
+    feature_cols = ['amount', 'loan_year', 'payments', 'duration']
+    x = train.loc[:, feature_cols]
+    y = train.status
+
+    log_reg = LogisticRegression()
+    log_reg.fit(x, y)
+
+    test = read_frame("loan_comp")
+    x_new = test.loc[:, feature_cols]
+    new_pred_class = log_reg.predict(x_new)
+
+    kaggle_data = pd.DataFrame({'Id': test.loan_id, 'Predicted': log_reg.predict_proba(x_new)[:, 0]}).set_index('Id')
+    kaggle_data.to_csv('./data/submission/sub-linear.csv')
+
+
+if __name__ == "__main__":
+    simple()
