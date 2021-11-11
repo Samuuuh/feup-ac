50d663a13a51440dd0615becc452c128c4b8ecf5
Juliane Marubayashi <juliane.marubayashi@gmail.com>
2021-11-11 20:44:11 +0000

    Modify: score to auc

--- prediction/__main__.py
+++ prediction/__main__.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-from prediction.models.model import Model
-from prediction.models.regression import Regression
+from models.model import Model
+from models.regression import Regression
 
 
 def read_frame(name: str) -> pd.DataFrame:
@@ -37,21 +37,22 @@ def search_grid_cv():
     train, test = train_test_split(development, test_size=0.2)
 
     # Train the model
-    regression = Regression(['amount', 'loan_year', 'payments', 'duration', 'loan_month'], 'status')
+    regression = Regression(['amount', 'loan_year', 'payments', 'duration'], 'status')
     regression.train(train)
 
     # Test the model
     prediction = regression.test(test)
     expected = Model.get_expected(test, "loan_id", "status")
-    print("Score: ", regression.score(expected, prediction))
+    regression.print_auc_sklearn(expected, prediction)
     #regression.plot_roc()
 
     # Apply to competition data
     competition = read_frame("loan_comp")
     prediction = regression.test(competition)
-    prediction.to_csv('./data/submission/sub-linear.csv')
+    prediction.to_csv('./data/submission/regression-grid.csv')
 
 
-    
+
 if __name__ == "__main__":
     simple()
+    search_grid_cv()
