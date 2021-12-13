b52757fa9ab248d56eca5251e7dc2aa06d58541d
Alexandre Abreu <xbreu@tuta.io>
2021-12-13 14:18:41 +0000

    add: new module for descriptive models

---
+++ src/algorithm/description/__main__.py
@@ -0,0 +1,28 @@
+import configparser
+import os
+import pandas as pd
+import sys
+
+from ..consts import ModelType
+from ..logger import Logger
+from ...preprocessing.utils import read_cleaned_csv, read_preprocessed_csv
+from ..__main__ import build, convert_status
+from .k_means import k_means
+
+import sqlite3
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        path = "/../../../config/config.ini"
+    else:
+        path = "/../../../" + sys.argv[1]
+    filepath = os.path.dirname(os.path.abspath(__file__)) + path
+
+    if os.path.isfile(filepath):
+        parser = configparser.ConfigParser()
+        parser.read(filepath)                       # Get's the config file.
+        df = build(parser)                          # Builds the dataframe.
+        df[0] = convert_status(df[0])
+        k_means(df)
+    else:
+        Logger.print_err("No config parser in this folder.")
---
+++ src/algorithm/description/k_means.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+def k_means(df: pd.DataFrame) -> None:
+    print(df)
